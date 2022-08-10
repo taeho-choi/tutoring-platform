82cc09049c6fe596ecf582e3ba88ae0e8f10414f
Choi Taeho <taehodev@naver.com>
2022-08-11 00:49:24 +0900

    make Loginpage ID, Password input, LoginBtn

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,7 +3,12 @@ import "./LoginPage.css";
 const LoginPage = () => {
   return (
     <div className="LoginPage">
-      <div className="LoginBox"></div>
+      <div className="LoginBox">
+        <div className="Logo">PROJECT_NAME</div>
+        <input placeholder="ID"></input>
+        <input placeholder="Password"></input>
+        <button className="loginBtn">Sign in</button>
+      </div>
     </div>
   );
 };
