af2f0adacbfc05204438074a8878643f83454bbb
Choi Taeho <taehodev@naver.com>
2022-08-15 23:52:44 +0900

    pretendard font load

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,11 +4,9 @@ const LoginPage = () => {
   return (
     <div className="LoginPage">
       <div className="LoginBox">
-        <div className="Logo">
-          <a>S</a>CHOLAR
-        </div>
+        <div className="Logo">Sign in</div>
         <input placeholder="ID"></input>
-        <input placeholder="Password"></input>
+        <input placeholder="Password" type={"password"}></input>
         <button className="loginBtn">Sign in</button>
       </div>
     </div>
