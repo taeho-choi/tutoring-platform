5ebf0924c9d2eeb5e93d40b915c6afdb759058d6
Choi Taeho <taehodev@naver.com>
2022-08-21 01:10:01 +0900

    add teacher profile page base

--- src/Teacher.js
+++ src/Teacher.js
@@ -7,8 +7,15 @@ const Teacher = () => {
       <div className="topArea">
         <div className="logo">LOGO</div>
         <div className="pageInfo">|　Teacher</div>
+        <div className="hello">
+          Hello, <a>TaehoChoi</a>
+        </div>
       </div>
-      <div className="box"></div>
+      <div className="bg">
+        <div className="box">
+          <img id="profile" src="img/profile.jpg" />
+        </div>
+      </div>
     </div>
   );
 };
